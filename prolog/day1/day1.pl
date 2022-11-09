e9822c6f1b2e62088c2f6d15d9d84cc7c999225f
Steven van Dijk <email@stevenvandijk.com>
2022-11-09 15:30:14 -0700

    day activity

---
+++ prolog/day1/day1.pl
@@ -0,0 +1,22 @@
+plays(bob, guitar).
+plays(ted, guitar).
+plays(ted, bass).
+plays(ann, sax).
+plays(sue, trumpet).
+plays(jim, bass).
+plays(jim, keyboard).
+plays(jen, drums).
+plays(jen, xylophone).
+
+genre(bob, rock).
+genre(bob, metal).
+genre(ted, blues).
+genre(ann, jazz).
+genre(sue, jazz).
+genre(jim, rock).
+genre(jim, metal).
+genre(jim, jazz).
+genre(jen, rock).
+genre(jen, metal).
+genre(jen, jazz).
+genre(jen, classical).
